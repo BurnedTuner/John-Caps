41a14cb5c06a692ff8458933d7c577892bfa6179
BurnedTuner <ainol0912@gmail.com>
2026-08-27 13:14:29 +0300

    main menu

--- Assets/Source/Scripts/UI/DeckPanelUI.cs
+++ Assets/Source/Scripts/UI/DeckPanelUI.cs
@@ -912,6 +912,6 @@ public class DeckPanelUI : MonoBehaviour
     {
         if (_countText == null) return;
         int count = CurrentDeckCount;
-        _countText.text = count < 10 ? $"0{count}" : $"{count}";
+        _countText.text = count < 10 ? $"{count}" : $"{count}";
     }
 }
